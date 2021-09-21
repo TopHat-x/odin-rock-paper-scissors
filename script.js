ae7ed70e51f6344e822cb721e35c1cb70a257a8e
TopHat-x <Brennan.Berryman@outlook.com>
2021-09-20 18:09:27 -0600

    Made functional game. It needs tidying up.

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let round = 1;
+let computerScore = 0;
+let playerScore = 0;
+let computerChoice;
+let playerChoice;
+let roundWinner;
+
+const playerScoreText = document.querySelector("#player-score");
+const computerScoreText = document.querySelector("#computer-score");
+const roundText = document.querySelector("#round");
+const results = document.querySelector("#results");
+
+const rockButton = document.querySelector("#rock-button");
+rockButton.addEventListener("click", () => playRound("rock"));
+
+const paperButton = document.querySelector("#paper-button");
+paperButton.addEventListener("click", () => playRound("paper"));
+
+const scissorsButton = document.querySelector("#scissors-button");
+scissorsButton.addEventListener("click", () => playRound("scissors"));
+
+const hodorButton = document.querySelector("#hodor-button");
+hodorButton.addEventListener("click", hodor);
+hodorButton.addEventListener("click", () => hodor("Hodor")); /* This works */
+/* hodorButton.addEventListener("click", hodor("Hodor")); /* This doesn't work */
+/* hodorButton.addEventListener("click", ("Hodor") => hodor()); /* This doesn't work */
+
+function updateScore() {
+    playerScoreText.textContent = "PLAYER: " + playerScore;
+    computerScoreText.textContent = "COMPUTER: " + computerScore;
+    roundText.textContent = "ROUND " + round;
+
+    if(computerScore >= 3){
+        results.textContent = "COMPUTER WINS!!!";
+    } else if (playerScore >=3){
+        results.textContent = "YOU'RE WINNER!";
+    }
+
+}
+
 function computerPlay() {
     let computerNumber = randomInt(3);
     let computerChoice;
@@ -13,93 +53,66 @@ function computerPlay() {
     return computerChoice;
 }
 
-function playerSelect() {
-    let playerChoice;
-    while (true) {
-        playerChoice = prompt("Choose rock, paper, or scissors!");
-        playerChoice = playerChoice.toLowerCase();
-    
-        if (playerChoice === 'rock'||playerChoice === 'paper'||playerChoice === "scissors"){
-            break;
-        }
-    }
-    return playerChoice;
-}
-
 function randomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
     let winner;
     if (playerSelection === 'rock'){
         if (computerSelection === 'rock'){
-            console.log('You both chose rock. Tie!');
-            winner = 'Nobody';
+            results.textContent ='You both chose rock. Tie!';
         } else if (computerSelection === 'paper'){
-            console.log('You chose rock, and computer chose paper. Computer wins!');
-            winner = 'Computer';
+            results.textContent ='You chose rock, and computer chose paper. Computer wins!';
+            computerScore++;
+            round++;
+            updateScore()
         } else if (computerSelection === 'scissors'){
-            console.log('You chose rock, and computer chose scissors. You\'re winner!');
-            winner = 'Player';
+            results.textContent ='You chose rock, and computer chose scissors. You\'re winner!';
+            playerScore++;
+            round++
+            updateScore()
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            console.log('You chose paper, and computer chose rock. You win!');
-            winner = 'Player';
-
+            results.textContent ='You chose paper, and computer chose rock. You win!';
+            playerScore++;
+            round++
+            updateScore()
         } else if (computerSelection === 'paper') {
-            console.log('You both chose paper. Tie!');
-            winner = 'Nobody';
+            results.textContent ='You both chose paper. Tie!';
         } else if (computerSelection === 'scissors') {
-            console.log('You chose paper, and computer chose scissors. Computer wins!');
-            winner = 'Computer';
+            results.textContent ='You chose paper, and computer chose scissors. Computer wins!';
+            computerScore++;
+            round++;
+            updateScore()
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            console.log('You chose scissors, and computer chose rock. Computer wins!');
-            winner = 'Computer';
-        } else if (computerSelection === 'paper') {
-            console.log('You chose scissors, and computer chose paper. You win!');
-            winner = 'Player';
-        } else if (computerSelection === 'scissors') {
-            console.log('You both chose scissors. Tie!');
-            winner = 'Nobody';
-        }
-    }
-    return winner;
-}
-
-function game() {
-    let round = 1;
-    let computerScore = 0;
-    let playerScore = 0;
-    let computerChoice;
-    let playerChoice;
-    let roundWinner;
-
-    console.log('This is a best of 5 series of Rock, Paper, Scissors!');
-
-    while (computerScore < 3 && playerScore < 3) {
-        console.log('The score is Computer ' + computerScore + ', Player ' + playerScore + '. Round ' + round + ' ... FIGHT!');
-        roundWinner = 'Nobody';
-        while (roundWinner === 'Nobody'){
-            computerChoice = computerPlay();
-            playerChoice = playerSelect();
-            roundWinner = playRound(playerChoice, computerChoice);
-        }
-        if (roundWinner === 'Computer'){
+            results.textContent ='You chose scissors, and computer chose rock. Computer wins!';
             computerScore++;
-        } else if (roundWinner === 'Player'){
+            round++;
+            updateScore()
+        } else if (computerSelection === 'paper') {
+            results.textContent ='You chose scissors, and computer chose paper. You win!';
             playerScore++;
+            round++
+            updateScore()
+        } else if (computerSelection === 'scissors') {
+            results.textContent ='You both chose scissors. Tie!';
         }
-        round++;
-    }
-
-    if (computerScore === 3) {
-        console.log('GAME OVER. COMPUTER WINS 3 to ' + playerScore);
-    } else if (playerScore === 3) {
-        console.log('GAME OVER. PLAYER WINS 3 to ' + computerScore);
-
     }
 }
+
+function hodor(hodorIn) {
+    if(hodorIn === "Hodor"){
+        console.log("Hodor");
+    } else {
+        console.log(":(");
+    }
+
+}
+
+
+
